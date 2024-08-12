c824871b7440c506d598a762388998c9ff8a458b
poniec <pawel96mac@gmail.com>
2024-08-12 12:23:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,11 +13,11 @@
     var orderedOverlayMaps = [];
 
     // Dodanie warstw mapy
-OSM": L.tileLayer('https://api.maptiler.com/maps/openstreetmap/{z}/{x}/{y}@2x.jpg?key=pDM24DBsryBDrIeZmRKb', {
+OSM": L.tileLayer('https://api.maptiler.com/maps/openstreetmap/?key=pDM24DBsryBDrIeZmRKb#0.7/22.80535/2.86559', {
             maxZoom: 30,
             attribution: '© OpenStreetMap contributors'
         }),
-        "Satelita": L.tileLayer('https://api.maptiler.com/maps/satellite/{z}/{x}/{y}@2x.jpg?key=pDM24DBsryBDrIeZmRKb', {
+        "Satelita": L.tileLayer('https://api.maptiler.com/maps/satellite/?key=pDM24DBsryBDrIeZmRKb#1.0/0.00000/0.00000', {
             maxZoom: 30,
             attribution: '© MapTiler contributors'
         })  };
